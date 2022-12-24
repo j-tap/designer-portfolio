2fe339afcde16be64d26d558a05e6831e043f2af
j-tap <j-tap@ya.ru>
2022-12-24 14:37:59 +0300

    feat (project): styles for contacts

--- frontend/pages/contacts/index.vue
+++ frontend/pages/contacts/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <h1>Contacts</h1>
-    <ul>
-      <li v-for="item in contacts" :key="item.name">
-        <a :href="item.link" target="_blank" rel="nofollow">{{ item.name }}</a>
+  <div class="page-contacts">
+    <h1 class="page-contacts__title page-title">
+      <span class="page-title__item">Контакты</span>
+      <span class="page-title__item">Контакты</span>
+    </h1>
+    <ul class="page-contacts__lis contacts-list">
+      <li class="contacts-list__item" v-for="item in contacts" :key="item.name">
+        <span>{{ item.name }}</span>
+        <a class="contacts-list__link" :href="item.link" target="_blank" rel="nofollow">{{ item.name }}</a>
+        <span>{{ item.name }}</span>
       </li>
     </ul>
   </div>

20fbe6455296fbe615671f4a517ea8a0b6d58d43
j-tap <j-tap@ya.ru>
2022-12-25 08:01:12 +0300

    feat (project): create marquee, fix styles

--- frontend/pages/index/index.vue
+++ frontend/pages/index/index.vue
@@ -1,46 +1,54 @@
 <template>
   <div class="page-main">
-    <ClientOnly>
-      <TitleOutline tag="span">ui/ux designer</TitleOutline>
-    </ClientOnly>
-    <h1 class="page-main__title">
-      <TitleOutline tag="div">Портфолио</TitleOutline>
-      <div>ui/ux designer</div>
-      <TitleOutline tag="div">Конойко Диана</TitleOutline>
-    </h1>
-
-    <h2>тут я собрала все свои последние клевые проекты</h2>
-    <ClientOnly>
+    <section class="page-main__section-head">
+      <div class="marquee">
+        <div class="marquee__inner">
+          <TitleOutline tag="span">ui/ux designer</TitleOutline>
+          <TitleOutline tag="span">ui/ux designer</TitleOutline>
+          <TitleOutline tag="span">ui/ux designer</TitleOutline>
+          <TitleOutline tag="span">ui/ux designer</TitleOutline>
+        </div>
+      </div>
+      <h1 class="page-main__title">
+        <TitleOutline tag="div">Портфолио</TitleOutline>
+        <div>ui/ux designer</div>
+        <TitleOutline tag="div">Конойко Диана</TitleOutline>
+      </h1>
+      <h2>тут я собрала все свои последние клевые проекты</h2>
       <TitleOutline tag="div">ui/ux designer</TitleOutline>
-    </ClientOnly>
-    <h3>О моем опыте</h3>
-    <p>В сфере дизайна я более 3х лет. За это время мне удалось поработать с разносторонними проектами, как мелкие стартапы, так и масштабные.</p>
+    </section>
+    <section class="page-main__section-info">
+      <h3>О моем опыте</h3>
+      <p>В сфере дизайна я более 3х лет. За это время мне удалось поработать с разносторонними проектами, как мелкие стартапы, так и масштабные.</p>
 
-    <TitleOutline tag="h3">инструменты</TitleOutline>
-    <div>
-      <p>Я работаю в программах:</p>
-      <ul>
-        <li>Figma;</li>
-        <li>Adobe пакет;</li>
-        <li>Axure;</li>
-        <li>Blender;</li>
-        <li>И многие другие.</li>
-      </ul>
-    </div>
-    <TitleOutline tag="h3">Навыки</TitleOutline>
-    <div>
-      <ul>
-        <li>Имеются знание и опыт принципов и методологий проектирования сложных систем и интерфейсов, atomic design.</li>
-        <li>Опыт работы в качестве UX\UI дизайнера.</li>
-        <li>Навык проектирования, прототипирования и реализации интерфейсов взаимодействия с пользователями в вебе и на мобильных устройствах.</li>
-        <li>Навык анимации и 3D проектирования.</li>
-      </ul>
-    </div>
+      <TitleOutline tag="h3">инструменты</TitleOutline>
+      <div>
+        <p>Я работаю в программах:</p>
+        <ul>
+          <li>Figma;</li>
+          <li>Adobe пакет;</li>
+          <li>Axure;</li>
+          <li>Blender;</li>
+          <li>И многие другие.</li>
+        </ul>
+      </div>
+      <TitleOutline tag="h3">Навыки</TitleOutline>
+      <div>
+        <ul>
+          <li>Имеются знание и опыт принципов и методологий проектирования сложных систем и интерфейсов, atomic design.</li>
+          <li>Опыт работы в качестве UX\UI дизайнера.</li>
+          <li>Навык проектирования, прототипирования и реализации интерфейсов взаимодействия с пользователями в вебе и на мобильных устройствах.</li>
+          <li>Навык анимации и 3D проектирования.</li>
+        </ul>
+      </div>
 
-    <TitleOutline tag="h3">тоже важное</TitleOutline>
-    <p>Есть базовые знания html/css и большой опыт взаимодействия с разработчиками, что упрощает процесс работы.</p>
+      <TitleOutline tag="h3">тоже важное</TitleOutline>
+      <p>Есть базовые знания html/css и большой опыт взаимодействия с разработчиками, что упрощает процесс работы.</p>
+    </section>
 
-    <PortfolioCategories :items="categories"/>
+    <section class="page-main__section-portfolio">
+      <PortfolioCategories :items="categories" />
+    </section>
   </div>
 </template>
 

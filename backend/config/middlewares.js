422eb36a9142c84a736341ec2f3265cef5deaadf
j-tap <j-tap@ya.ru>
2023-01-15 02:41:08 +0300

    fix (app): build

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,18 +1,22 @@
-module.exports = ({ env }) => [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::poweredBy',
-  {
-    name: 'strapi::cors',
-    config: {
-      headers: ['accept-language', 'Token', 'Content-Type', 'Authorization', 'Accept', 'Origin'],
-      origin: env('CORS_ORIGIN')?.split(',') || [],
+module.exports = ({ env }) => {
+  console.log('CORS', env('CORS_ORIGIN'))
+  const corsOrigin = env('CORS_ORIGIN')?.split(',') || []
+  return [
+    'strapi::errors',
+    'strapi::security',
+    'strapi::poweredBy',
+    {
+      name: 'strapi::cors',
+      config: {
+        headers: '*',
+        origin: ['http://localhost:1337', ...corsOrigin],
+      },
     },
-  },
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+}

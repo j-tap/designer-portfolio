a150523cfde2821bffe3f4efa130df897ca788ff
j-tap <j-tap@ya.ru>
2023-01-14 05:42:08 +0300

    debug (plugin):

--- frontend/plugins/redirectAdmin.js
+++ frontend/plugins/redirectAdmin.js
@@ -2,6 +2,7 @@ export default defineNuxtPlugin(() => {
   addRouteMiddleware('admin', (to) => {
     if (to.fullPath === '/admin') {
       const runtimeConfig = useRuntimeConfig()
+      console.log(runtimeConfig)
       const url = `${runtimeConfig.strapi.url}/admin`
 
       window.open(url, '_blank')

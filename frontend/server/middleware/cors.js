1291c527f8e566d412008cc8db31964445613ec9
j-tap <j-tap@ya.ru>
2023-01-15 16:42:53 +0300

    fix (app): build

--- frontend/server/middleware/cors.js
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler((event) => {
-  setResponseHeaders(event, {
-    'Access-Control-Allow-Origin': process.env.STRAPI_URL,
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Headers': '*',
-    'Access-Control-Expose-Headers': '*'
-  })
-})

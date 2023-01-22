afee744494e7fb1be42e3208d629b1b139bb9554
j-tap <j-tap@ya.ru>
2023-01-22 23:36:07 +0300

    feat (app): add animate to image preloader, create icon component

---
+++ frontend/components/common/IconBase/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <i :class="classes">
+    <Component :is="iconComponent" />
+  </i>
+</template>
+
+<script setup>
+import { kebabToPascalCase } from '~/utils/formatData'
+
+const props = defineProps({
+  name: {
+    type: String,
+    default: 'unknown'
+  },
+})
+
+const icon = computed(() => `Icon${kebabToPascalCase(props.name)}`)
+const iconComponent = computed(() =>
+    defineAsyncComponent(() => import(`./icons/${icon.value}.vue`)))
+
+const classes = computed(() => [
+  'icon',
+  `icon_${name}`
+])
+</script>
+
+<style lang="scss" src="./style.scss" scoped />

95a72943c0c0da04ff56a676d752f616a2b83156
j-tap <j-tap@ya.ru>
2023-02-18 15:17:56 +0300

    feat (ProjectColors): sorting colors by luminance

--- frontend/components/sections/_project/ProjectColors/index.vue
+++ frontend/components/sections/_project/ProjectColors/index.vue
@@ -3,7 +3,7 @@
     <ScrollBlock>
       <ul class="project-colors__list">
         <li
-          v-for="item in props.items"
+          v-for="item in colorsList"
           :key="item.id"
           class="project-colors__item"
         >
@@ -21,6 +21,40 @@ import { ScrollBlock } from '~/components/common'
 const props = defineProps({
   items: Array,
 })
+
+const colorsList = computed(() => {
+  return props.items.sort((a, b) => {
+    const v1 = rgbToLuminance(hexToRgbArr(a.color))
+    const v2 = rgbToLuminance(hexToRgbArr(b.color))
+
+    return v1 > v2 ? 1 : (v1 < v2 ? -1 : 0)
+  })
+})
+
+function rgbToLuminance (rgb) {
+  return Math.sqrt(.299 * rgb[0] * rgb[0] + .587 * rgb[1] * rgb[1] + .114 * rgb[2] * rgb[2])
+}
+
+function hexToRgbArr (color = null) {
+  if (color) {
+    let s = color.toString()
+    s = s.replace('#', '')
+
+    if (s.length !== 6) {
+      s = s.split('').map(t =>  t + t).join('')
+    }
+
+    const hexArr = s.match(/.{1,2}/g)
+
+    return [
+      parseInt(hexArr[0], 16),
+      parseInt(hexArr[1], 16),
+      parseInt(hexArr[2], 16),
+    ]
+  }
+
+  return null
+}
 </script>
 
 <style lang="scss" src="./style.scss" scoped />

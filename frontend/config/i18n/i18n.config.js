c8d8cb62243d95af95b30aceeb6c2573f364a85e
j-tap <j-tap@ya.ru>
2023-12-08 15:19:30 +0700

    fix (app-frontend): try fix i18n

--- frontend/config/i18n/i18n.config.js
+++ frontend/config/i18n/i18n.config.js
@@ -2,6 +2,7 @@ import pluralRules from './i18n-plural'
 import { defaultLocale } from './index'
 
 export default defineI18nConfig((_nuxt) => ({
+  lazy: false,
   legacy: false,
   fallbackLocale: defaultLocale,
   pluralRules,

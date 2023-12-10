465ec83be20da9ab0d14c0f54afa3be9f03a94d2
j-tap <j-tap@ya.ru>
2023-12-10 14:37:16 +0700

    feat (frontend): adding ya metrika

---
+++ frontend/plugins/yandex-metrika.js
@@ -0,0 +1,19 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  if (process.client) {
+    (function(m,e,t,r,i,k,a){
+      m[i]=m[i]||function(){
+        (m[i].a=m[i].a||[]).push(arguments)
+      };
+      m[i].l=1*new Date()
+      k=e.createElement(t), a=e.getElementsByTagName(t)[0], k.async=1, k.src=r, a.parentNode.insertBefore(k,a)
+    })(window, document, 'script', 'https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js', 'ym')
+
+    ym(process.env.YANDEX_METRIKA_ID, 'init', {
+      clickmap:true,
+      trackLinks:true,
+      accurateTrackBounce:true,
+      webvisor:true,
+      trackHash:true
+    })
+  }
+})

480aaaf3d4a544fc5664f803b28de67ab171b79a
j-tap <j-tap@ya.ru>
2024-03-26 18:57:08 +0700

    feat (frontend/project): project presentation

--- frontend/components/sections/_project/ProjectPresentation/index.vue
+++ frontend/components/sections/_project/ProjectPresentation/index.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="project-presentation">
-    <pre>{{data}}</pre>
+    <ImgBlock
+      v-for="img in images"
+      :key="img.id"
+      :src="urlFile(img.url)"
+      :src-lite="urlFile(img.formats.large.url)"
+      :blurhash="img.blurhash"
+      :width="img.width"
+      :height="img.height"
+      :alt="img.slug"
+      class="project-presentation__img"
+      itemprop
+    />
   </div>
 </template>
 
 <script setup>
-const props = defineProps({
-  data: Object,
+import { ImgBlock } from '~/components/common'
+
+defineProps({
+  images: Array,
 })
 </script>
+
+<style lang="scss" src="./style.scss" scoped />

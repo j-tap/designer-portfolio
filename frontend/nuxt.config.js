9c4be88de038d4709fdda69b4f91439462dbaff6
j-tap <j-tap@ya.ru>
2024-07-17 11:48:58 -0300

    fix (app): upgrade

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -9,62 +9,72 @@ const isDev = process.env.NODE_ENV !== 'production'
 export default defineNuxtConfig({
 	// debug: isDev,
 	devtools: { enabled: isDev },
+
 	build: {
 		transpile: ['vue-i18n'],
 	},
+
 	nitro: {
 		prerender: {
 			// crawlLinks: true,
 			// routes: ['/', 'sitemap.xml'],
-			ignore: ['/card'],
-		},
+			ignore: ['/card']
+		}
 	},
+
 	components: {
-		dirs: [],
+		dirs: []
 	},
+
 	runtimeConfig: {
 		strapi: {
-			url: strapi.url,
+			url: strapi.url
 		},
 		public: {
 			baseURL,
 			yandexMetrikaId: process.env.YANDEX_METRIKA_ID,
-			NODE_ENV: process.env.NODE_ENV,
-		},
+			NODE_ENV: process.env.NODE_ENV
+		}
 	},
+
 	vite: {
 		css: {
 			preprocessorOptions: {
 				scss: {
-					additionalData: '@use "~/assets/style/global.scss" as *;',
-				},
-			},
-		},
+					additionalData: '@use "~/assets/style/global.scss" as *;'
+				}
+			}
+		}
 	},
-  app: {
-    head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1.0',
+
+	app: {
+		head: {
+			charset: 'utf-8',
+			viewport: 'width=device-width, initial-scale=1.0',
 			meta: [
 				{ name: 'google-site-verification', content: process.env.GOOGLE_SITE_VERIFICATION },
 				{ name: 'yandex-verification', content: process.env.YANDEX_SITE_VERIFICATION },
-				{ 'http-equiv': 'Cache-Control', content: `max-age=${process.env.CACHE_TIME || '60'}, must-revalidate` },
-			],
-    },
-	  pageTransition: { name: 'page', mode: 'out-in' },
-		layoutTransition: { name: 'layout', mode: 'out-in' },
-  },
-	site: {
-		url: baseURL,
+				{ 'http-equiv': 'Cache-Control', content: `max-age=${process.env.CACHE_TIME || '60'}, must-revalidate` }
+			]
+		},
+		pageTransition: { name: 'page', mode: 'out-in' },
+		layoutTransition: { name: 'layout', mode: 'out-in' }
 	},
-  modules: [
+
+	site: {
+		url: baseURL
+	},
+
+	modules: [
 		'@nuxt/devtools',
 		'@nuxtjs/i18n',
 		'@nuxtjs/sitemap',
 		'@nuxtjs/strapi',
 		'@pinia/nuxt',
-  ],
-  strapi,
+	],
+
+	strapi,
 	i18n,
 	sitemap,
+	compatibilityDate: '2024-07-17'
 })
